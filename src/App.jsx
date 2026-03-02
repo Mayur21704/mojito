2b7f480c2256043556900061d34e93067994581f
Mayur <mayur72104@gmail.com>
2026-03-02 16:39:20 +0530

    Start again

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import React from "react";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import gsap from "gsap";
-import Navbar from "./components/Navbar";
-import Hero from "./components/Hero";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <main>
-      <Navbar />
-      <Hero />
-      <div className="h-screen">
-
-      </div>
-    </main>
+    <div>
+      <h1 className="text-3xl text-red-200">H1</h1>
+    </div>
   );
 };
 
